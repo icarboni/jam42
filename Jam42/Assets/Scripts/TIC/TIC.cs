09229f159dac4bb7f0c1309e36816b2c0ef6ef2c
Trollan <manuelsanchezies@gmail.com>
2022-09-23 18:06:58 +0200

    Cerbero + stunning

--- Jam42/Assets/Scripts/TIC/TIC.cs
+++ Jam42/Assets/Scripts/TIC/TIC.cs
@@ -14,6 +14,9 @@ public class TIC : MonoBehaviour
     IEnumerator Tic(Collider2D _other)
     {
         _other.GetComponent<playerMovement>().stunned = true;
+        _other.GetComponent<Animator>().SetTrigger("Stunned");
+        //_other.GetComponentInChildren<Animator>().SetTrigger("CerbSpawn");
+        _other.transform.GetChild(1).GetComponent<Animator>().SetTrigger("CerbSpawn");
         yield return new WaitForSeconds(2);
         _other.GetComponent<playerMovement>().stunned = false;
         Destroy(this.gameObject);
