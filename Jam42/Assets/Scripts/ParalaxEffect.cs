cb5665a86b99a2c9dca45af5d447c09655222e77
Trollan <manuelsanchezies@gmail.com>
2022-09-23 02:22:10 +0200

    Fixed Paralax

--- Jam42/Assets/Scripts/ParalaxEffect.cs
+++ Jam42/Assets/Scripts/ParalaxEffect.cs
@@ -18,16 +18,27 @@ public class ParalaxEffect : MonoBehaviour
     }
     public void parallax() 
     {
-        if (playerMScript.stunned == false && lookVelocity.velocity.x < 0)
-        paralaxTransform[0].transform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
-        if (playerMScript.stunned == false && lookVelocity.velocity.x < 0)
-        paralaxTransform[1].transform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+        if (playerMScript.stunned == false && lookVelocity.velocity.x < -1)
+            paralaxTransform[0].transform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+
+        if (playerMScript.stunned == false && lookVelocity.velocity.x > 1)
+            paralaxTransform[0].transform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+
+        if (playerMScript.stunned == false && lookVelocity.velocity.x < -1)
+            paralaxTransform[1].transform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+
+        if (playerMScript.stunned == false && lookVelocity.velocity.x > 1)
+            paralaxTransform[1].transform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+
         if (paralaxTransform[0].transform.localPosition.x <= -1920f && Input.GetAxis("Horizontal") > 0)
             paralaxTransform[0].transform.localPosition = new Vector3(1920f, paralaxTransform[0].transform.localPosition.y, paralaxTransform[0].transform.localPosition.z);
+
         if (paralaxTransform[1].transform.localPosition.x <= -1920f && Input.GetAxis("Horizontal") > 0)
             paralaxTransform[1].transform.localPosition = new Vector3(1920f, paralaxTransform[0].transform.localPosition.y, paralaxTransform[0].transform.localPosition.z);
+
         if (paralaxTransform[0].transform.localPosition.x >= 1920f && Input.GetAxis("Horizontal") < 0)
             paralaxTransform[0].transform.localPosition = new Vector3(-1920f, paralaxTransform[0].transform.localPosition.y, paralaxTransform[0].transform.localPosition.z);
+
         if (paralaxTransform[1].transform.localPosition.x >= 1920f && Input.GetAxis("Horizontal") < 0)
             paralaxTransform[1].transform.localPosition = new Vector3(-1920f, paralaxTransform[0].transform.localPosition.y, paralaxTransform[0].transform.localPosition.z);
     }
