a3b6756761f8f93b3d5f36111d9a4934c0692bad
Ilenia <ile.carboni@gmail.com>
2022-09-24 21:25:13 +0200

    fix world

--- Jam42/Assets/Scripts/WorldGeneration.cs
+++ Jam42/Assets/Scripts/WorldGeneration.cs
@@ -15,32 +15,32 @@ public class WorldGeneration : MonoBehaviour
     void Start()
     {
 
-
-        for (int i = 1; i < platformNumber; i++)
+        do
+            numRandom = Random.Range(0, 5);
+        while (numRandom != 0 && numRandom != 4);
+        for (int i = 0; i < platformNumber; i++)
         {
-            do
-                numRandom = Random.Range(0, 5);
-            while (numRandom != 0 && numRandom != 4);
-            Instantiate(ground[numRandom], firstPosition.position + new Vector3(17.75f * (0), 0, 0), Quaternion.identity, parent.transform);
-
             Instantiate(ground[numRandom], firstPosition.position + new Vector3(17.75f * (i), 0, 0), Quaternion.identity, parent.transform);
             if (numRandom == 0)
             {
-                Instantiate(blocks[numRandom].platforms[Random.Range(0, 5)], firstPosition.position + new Vector3(17.75f * (i), -1, 0), Quaternion.identity, firstPosition);
+                if (i != 0)
+                    Instantiate(blocks[numRandom].platforms[Random.Range(0, 5)], firstPosition.position + new Vector3(17.75f * (i), -1, 0), Quaternion.identity, firstPosition);
                 do
                     numRandom = Random.Range(0, 5);
                 while (numRandom != 0 && numRandom != 3);
             }
             else if (numRandom == 1)
             {
-                Instantiate(blocks[numRandom].platforms[Random.Range(0,9)], firstPosition.position + new Vector3(17.75f * (i), 0.4f, 0), Quaternion.identity, firstPosition);
+                if (i != 0)
+                    Instantiate(blocks[numRandom].platforms[Random.Range(0,9)], firstPosition.position + new Vector3(17.75f * (i), 0.4f, 0), Quaternion.identity, firstPosition);
                 do
                     numRandom = Random.Range(0, 5);
                 while (numRandom != 2 && numRandom != 4);
             }
             else if (numRandom == 2)
             {
-                Instantiate(blocks[numRandom].platforms[Random.Range(0, 3)], firstPosition.position + new Vector3(17.75f * (i), -0.4f, 0), Quaternion.identity, firstPosition);
+                if (i != 0)
+                    Instantiate(blocks[numRandom].platforms[Random.Range(0, 3)], firstPosition.position + new Vector3(17.75f * (i), -0.4f, 0), Quaternion.identity, firstPosition);
                 do
                     numRandom = Random.Range(0, 5);
                 while (numRandom != 1 && numRandom != 4);
@@ -51,11 +51,10 @@ public class WorldGeneration : MonoBehaviour
                     numRandom = Random.Range(0, 5);
                 while (numRandom != 1 && numRandom != 2);
             }
-            else
+            else if (numRandom == 4)
             {
                 numRandom = 0;
             }
-
 
         }
     }
