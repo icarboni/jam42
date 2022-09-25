9d1c11075a8cd6fe0cb6fb55f4879f5a9048dd71
Trollan <manuelsanchezies@gmail.com>
2022-09-26 00:42:51 +0200

    Trampolin sound and jump effect

--- Jam42/Assets/Scripts/TrampolinAnim.cs
+++ Jam42/Assets/Scripts/TrampolinAnim.cs
@@ -6,12 +6,19 @@ public class TrampolinAnim : MonoBehaviour
 {
     public GameObject trampolin;
     public GameObject trampolinExtend;
+    public AudioSource audiomanager;
+    public AudioClip trampolinSound;
+    public void Start()
+    {
+        audiomanager = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) 
         {
             trampolin.SetActive(true);
             trampolinExtend.SetActive(false);
+            audiomanager.PlayOneShot(trampolinSound, 1f);
         }
     }
 
