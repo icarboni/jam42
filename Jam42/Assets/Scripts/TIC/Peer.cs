703b0a1b983d819de3d10f088008689ba515830a
Trollan <manuelsanchezies@gmail.com>
2022-09-26 04:52:02 +0200

    Fixed animatiosn peer to peer

--- Jam42/Assets/Scripts/TIC/Peer.cs
+++ Jam42/Assets/Scripts/TIC/Peer.cs
@@ -6,22 +6,29 @@ public class Peer : MonoBehaviour
 {
     public float time = 2;
     public Vector2 minMaxPoints;
+    public Animator animPeer;
 
+    private void Start()
+    {
+        animPeer = GetComponent<Animator>();
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetKeyDown(KeyCode.E) && collision.GetComponent<playerMovement>().interactable)
         {
             PeerEject(collision);
-            Debug.Log("Peer");
         }
     }
     public void PeerEject(Collider2D _other) => StartCoroutine("Peers", _other);
 
     IEnumerator Peers(Collider2D _other)
     {
+        animPeer.SetBool("Chatting", true);
+            Debug.Log("Peer");
         _other.GetComponent<playerMovement>().stunned = true;
         yield return new WaitForSeconds(time);
         _other.GetComponent<playerMovement>().stunned = false;
+        animPeer.SetBool("Chatting", false);
         GameManager.instance.coins += (int)Random.Range(minMaxPoints.x, minMaxPoints.y);
         Destroy(this.gameObject);
         yield return null;
